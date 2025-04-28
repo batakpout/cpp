850e5c3b12d150f4c79b2ae9ad5f9996000f66a3
aamir <16462830+batakpout@users.noreply.github.com>
2025-04-28 13:40:20 +0530

    Create Problem_17.cpp

---
+++ dsalgo/leetcode/medium/Problem_17.cpp
@@ -0,0 +1,50 @@
+#include<iostream>
+#include<map>
+
+using namespace std;
+class Solution {
+public:
+    unordered_map<char, string> mp = {
+        {'2', "abc"},
+        {'3', "def"},
+        {'4', "ghi"},
+        {'5', "jkl"},
+        {'6', "mno"},
+        {'7', "pqrs"},
+        {'8', "tuv"},
+        {'9', "wxyz"}
+    };
+    void backtrack(const string& digits, vector<string>& result, int index, string current) {
+        if (index == digits.size()) {
+            result.push_back(current);
+            return;
+        }
+        string letters = mp[digits[index]];
+        for (char letter : letters) {
+            backtrack(digits, result, index + 1, current + letter);
+        }
+    }
+    vector<string> letterCombinations(string digits) {
+         if (digits.empty()) return {};
+            vector<string> result;
+            backtrack(digits, result, 0, "");
+            return result;
+    }
+};
+
+int main() {
+    string digits;
+    cout << "Enter digits (2-9): ";
+    cin >> digits;
+    
+    Solution solution;
+    vector<string> combinations = solution.letterCombinations(digits);
+    
+    cout << "Possible letter combinations:\n";
+    for (const string& comb : combinations) {
+        cout << comb << " ";
+    }
+    cout << endl;
+    
+    return 0;
+}
