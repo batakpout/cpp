adcb55141cfa65a36fa40136d2593fae3c0ed640
aamir <16462830+batakpout@users.noreply.github.com>
2025-04-28 12:56:31 +0530

    Update binary_strings.cpp

--- dsalgo/recursion/binary_strings.cpp
+++ dsalgo/recursion/binary_strings.cpp
@@ -10,6 +10,21 @@ int totalNoOfBinaryStrings(int n) {
 
 }
 
+/**
+time-complexity: 
+ * O(2^n) time complexity in the worst case. However, due to the constraint (where '1' can only follow '0'  or be at position 0), the actual number of strings generated will be less than 2^n.
+ * using memoisation in dp this can be reduced to linear complexity.
+ * using matrix exponentiation [CP] for linear recurrence i.e f(n) = f(n-1) + f(n-2), we can solve linear recurrence in logrithmic time, so using
+ matrix exponentiation we can reduce it down to O((k*k*k) Log N), k is no of terms on which this
+ N is depending i.e for 2 to power n, k is 2 so O(8 * LogN) --> uses matrices in maths
+
+Space Complexity:
+*Recursion stack depth: O(n) since the recursion goes n levels deep.
+The total space complexity is O(n) for the recursion stack plus O(n) for the string storage at each level, which simplifies to O(n).
+
+*Note that the overall space used by the final result array (storing all valid strings) would be O(k*n) where k is the number of valid strings generated, but this is separate from the recursive function's own space complexity.
+ */
+
 vector<string> result;
 
 void helper(string str, int n, int i) {
