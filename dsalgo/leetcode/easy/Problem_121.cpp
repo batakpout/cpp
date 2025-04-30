850d4f517f48f3f6cf5d91a661d913dbcaa7ad8a
aamir <16462830+batakpout@users.noreply.github.com>
2025-04-30 16:08:47 +0530

    Create Problem_121.cpp

---
+++ dsalgo/leetcode/easy/Problem_121.cpp
@@ -0,0 +1,36 @@
+/**
+ * Best time to buy and sell stock
+ * with one transaction
+ * O(n) time complexity
+ */
+#include<iostream>
+using namespace std;
+
+int buyAndSellStock(vector<int>& prices) {
+        int n = prices.size();
+        int mini = prices[0];
+        int maxProfit = 0;
+        int cost;
+        for(int i=0;i<n;i++) {
+            cost = prices[i] - mini;
+            maxProfit = max(maxProfit, cost);
+            mini = min(prices[i], mini);
+
+        }
+        return maxProfit;
+}
+
+int main() {
+    int size;
+    cout << "enter array size:";
+    cin >> size;
+    vector<int>prices(size);
+    cout << "enter " << size << " array elements: \n";
+    for(int i=0;i<size;i++) {
+        cin >> prices[i];
+    }
+
+    int res = buyAndSellStock(prices);
+    cout << "Profit: " << res << endl;
+    return 0;
+}
