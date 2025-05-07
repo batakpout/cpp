e7ba83016c027816653da6da513264df5262703d
aamir <16462830+batakpout@users.noreply.github.com>
2025-05-07 12:32:01 +0530

    Update reaarange_array_in_place.cpp

--- dsalgo/arrays/reaarange_array_in_place.cpp
+++ dsalgo/arrays/reaarange_array_in_place.cpp
@@ -7,6 +7,18 @@ using namespace std;
 /*
 TC: O(n)
 SC: O(1)
+Each number is between 0 and n-1 (inclusive), meaning they're all valid indices.
+You want to rearrange the array, such that each element at index i becomes arr[arr[i]].
+
+The Core Trick (Packing Two Numbers in One)
+
+arr = {3, 2, 0, 1}, n = 4
+after step-1 array becomes--> arr = {7, 2, 12, 9}
+Each number now holds:
+Old value: arr[i] % 4
+New value: arr[i] / 4
+Step 2: Extract the new value
+Now just divide all by n:
 */
 void rearrangeArray(int arr [], int n) {
 
