cc9d56aa2176b82f4d852c5e1fcafb0eee9fd7d8
aamir <16462830+batakpout@users.noreply.github.com>
2025-05-08 10:40:51 +0530

    Add files via upload

---
+++ dsalgo/2darray/snake_pattern.cpp
@@ -0,0 +1,36 @@
+#include<iostream>
+using namespace std;
+/**
+  O(rows * cols)
+Snake pattern:
+ 12 →  45 →  23 →  67
+ 2 ←  56 ←  34 ←  89
+ 78 →  19 →  35 →  91
+ 43 ←  8 ←  62 ←   4
+ */
+void snakePattern(vector<vector<int>> & matrix) {
+    int rows = matrix.size();
+    int cols = matrix[0].size();
+    for(int i=0;i<rows;i++) {
+        if(i%2 == 0) {
+            for(int j=0;j<cols;j++) {
+                cout << matrix[i][j] << " ";
+            }
+        } else {
+           for(int j=cols-1;j>=0;j--) {
+                cout << matrix[i][j] << " ";
+            }  
+        }
+        cout << endl;
+    }
+}
+int main() {
+vector<vector<int>> arr = {
+        {12, 45, 23, 67},
+        {89, 34, 56,  2},
+        {78, 19, 35, 91},
+        {4, 62,  8, 43}
+    };
+    snakePattern(arr);
+    return 0;
+}
