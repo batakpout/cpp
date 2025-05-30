2a70a23bfc6655b5ffae837da4e770af3e748520
aamir <16462830+batakpout@users.noreply.github.com>
2025-05-30 16:45:58 +0530

    Update find_words_formed_by_chars.cpp

--- dsalgo/strings/find_words_formed_by_chars.cpp
+++ dsalgo/strings/find_words_formed_by_chars.cpp
@@ -1,4 +1,6 @@
 /**
+ * LC, E:  1160 Find Words That Can Be Formed by Characters
+
 * You are given an array of strings words and a string chars. A string is good if it can be formed by characters from chars (each character can only be used once for each word in words).Return the sum of lengths of all good strings in words.
 Example 1: Input: words = ["cat","bt","hat","tree", "cac"], chars = "atach" Output: 6
 Explanation: The strings that can be formed are "cat" and "hat" so the answer is 3 + 3 = 6. No "cac" as chars have c only once
